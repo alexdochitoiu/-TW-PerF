7f2d306d2d4446a69baa2bcd10bd051b3ddba404
Alexandru Dochitoiu <alexandru.dochitoiu@info.uaic.ro>
2017-06-13 00:41:59 +0300

    Filters done

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -41,6 +41,7 @@ class IndexController extends Controller
                         ->where('tipTranzactie', '=', $request->get('tipTranzactie'))
                         ->get();
                 }
+
                 break;
 
             case 1:
@@ -80,6 +81,82 @@ class IndexController extends Controller
                 break;
         }
 
+        //Filtre
+        if($request->has('pretDeLa')) {
+            $var =  $request->get('pretDeLa');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    return $value->pret >= $var;
+                });
+        }
+
+        if($request->has('pretPanaLa')) {
+            $var =  $request->get('pretPanaLa');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    return $value->pret <= $var;
+                });
+        }
+
+        if($request->has('suprafataDeLa')) {
+            $var =  $request->get('suprafataDeLa');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    return $value->suprafata >= $var;
+                });
+        }
+
+        if($request->has('suprafataPanaLa')) {
+            $var =  $request->get('suprafataPanaLa');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    return $value->suprafata <= $var;
+                });
+        }
+
+        if($request->has('nrCamere') && $request->get('nrCamere') != 0) {
+            $var =  $request->get('nrCamere');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    return $value->numarCamere == $var;
+                });
+        }
+
+        if($request->has('compartimentare') && $request->get('compartimentare') != 0) {
+            $var =  $request->get('compartimentare');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    return $value->compartimentare == $var;
+                });
+        }
+
+        if($request->has('etaj') && $request->get('etaj') != 0) {
+            $var =  $request->get('etaj');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    if($var == "Parter") return $value->etaj == 0;
+                    return $value->etaj == $var;
+                });
+        }
+
+        if($request->has('anConstr') && $request->get('anConstr') != 0) {
+            $var =  $request->get('anConstr');
+            $anunturi = $anunturi->filter(
+                function ($value, $key) use (&$var){
+                    switch($var) {
+                        case "i1977":
+                            return $value->anConstructie < 1977;
+                        case "77-90":
+                            return $value->anConstructie >= 1977 && $value->anConstructie < 1990;
+                        case "90-00":
+                            return $value->anConstructie >= 1990 && $value->anConstructie < 2000;
+                        case "00-13":
+                            return $value->anConstructie >= 2000 && $value->anConstructie < 2013;
+                        case "d2013":
+                            return $value->anConstructie >= 2013;
+                    }
+                });
+        }
         include "smsGateway.php";
         $smsGateway = new SmsGateway('demo@smsgateway.me', 'password');
 
